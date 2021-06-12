6e145c7f6ccc6f6c34450bf8bfbd104cca4afb53
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-12 20:50:55 +0900

    수정2

--- ChatPrjTest/src/main/java/com/hyong/chatprj/test/ChatPrjTestApplication.java
+++ ChatPrjTest/src/main/java/com/hyong/chatprj/test/ChatPrjTestApplication.java
@@ -2,12 +2,20 @@ package com.hyong.chatprj.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatPrjTestApplication {
+public class ChatPrjTestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatPrjTestApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(ChatPrjTestApplication.class);
 	}
 
 }
