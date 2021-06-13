b19c1dcfc64e43d895c1efd323791ef7e65738ae
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-06-13 22:52:27 +0900

    t

--- ChatPrjTest/src/main/java/com/hyong/chatprj/test/controller/HomeController.java
+++ ChatPrjTest/src/main/java/com/hyong/chatprj/test/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.hyong.chatprj.test.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-	@RequestMapping("/jenkins-test")
+	@RequestMapping("/index")
 	public String test() {
-		return "잘실행되고 있음";
+		return "index";
 	}
 }
